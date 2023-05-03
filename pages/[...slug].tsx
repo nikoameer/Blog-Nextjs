d6078f1d765b6f5821e0c905a35593b674eee215
Nikolas Kapali <nikolas.kapali@exove.fi>
2023-05-03 14:56:46 +0300

    Format files according to settings in workspace

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,20 +1,21 @@
-import { GetStaticPathsResult, GetStaticPropsResult } from "next"
-import Head from "next/head"
-import { DrupalNode } from "next-drupal"
+import { GetStaticPathsResult, GetStaticPropsResult } from "next";
+import Head from "next/head";
+import { DrupalNode } from "next-drupal";
 
-import { drupal } from "lib/drupal"
-import { NodeArticle } from "components/node--article"
-import { NodeBasicPage } from "components/node--basic-page"
-import { Layout } from "components/layout"
+import { drupal } from "lib/drupal";
+import { NodeArticle } from "components/node--article";
+import { NodeBasicPage } from "components/node--basic-page";
+import { Layout } from "components/layout";
 
-const RESOURCE_TYPES = ["node--page", "node--article"]
+const RESOURCE_TYPES = ["node--page", "node--article"];
 
 interface NodePageProps {
-  resource: DrupalNode
+  resource: DrupalNode;
 }
 //console.log("Ayo?: ", process.env.NEXT_PUBLIC_DRUPAL_BASE_URL);
 export default function NodePage({ resource }: NodePageProps) {
-  if (!resource) return null
+  if (!resource) return null;
+  console.log("what is Resource?: ", resource);
 
   return (
     <Layout>
@@ -25,7 +26,7 @@ export default function NodePage({ resource }: NodePageProps) {
       {resource.type === "node--page" && <NodeBasicPage node={resource} />}
       {resource.type === "node--article" && <NodeArticle node={resource} />}
     </Layout>
-  )
+  );
 }
 
 // Get list of all available paths
@@ -33,46 +34,44 @@ export async function getStaticPaths(context): Promise<GetStaticPathsResult> {
   return {
     paths: await drupal.getStaticPathsFromContext(RESOURCE_TYPES, context),
     fallback: "blocking",
-  }
+  };
 }
 
 // Fetch data
 export async function getStaticProps(
   context
 ): Promise<GetStaticPropsResult<NodePageProps>> {
-  const path = await drupal.translatePathFromContext(context)
-
+  const path = await drupal.translatePathFromContext(context);
+  console.log("what is Context?: ", context);
   if (!path) {
     return {
       notFound: true,
-    }
+    };
   }
 
-  const type = path.jsonapi.resourceName
+  const type = path.jsonapi.resourceName;
 
-  let params = {}
+  let params = {};
   if (type === "node--article") {
     params = {
       include: "field_image,uid",
-    }
-    withAuth: true // Set the custom auth here.
+    };
+    withAuth: true; // Set the custom auth here.
   }
-  
+
   // Fetch one resource
   const resource = await drupal.getResourceFromContext<DrupalNode>(
     path,
     context,
-    {
-      params,
-    }
-  )
+    { params }
+  );
 
   // At this point, we know the path exists and it points to a resource.
   // If we receive an error, it means something went wrong on Drupal.
   // We throw an error to tell revalidation to skip this for now.
   // Revalidation can try again on next request.
   if (!resource) {
-    throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`)
+    throw new Error(`Failed to fetch resource: ${path.jsonapi.individual}`);
   }
 
   // If we're not in preview mode and the resource is not published,
@@ -80,12 +79,12 @@ export async function getStaticProps(
   if (!context.preview && resource?.status === false) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
     props: {
       resource,
     },
-  }
+  };
 }
