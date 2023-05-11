e503f09febd85592c2afa6262e2da4a79e7235e2
Nikolas Kapali <nikolas.kapali@exove.fi>
2023-05-11 13:31:57 +0300

    Tailwind custom colors and body color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#fff",
+      black: "#000",
+      blogGreen: "#81DD22",
+      blogBlue: "#2281DD",
+      blogPink: "#DD2281",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-}
+};
